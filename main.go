80b44c9a7836c455ba46de45bf8e27d10531a35d
aarjun <aarjun.re75@gmail.com>
2023-08-05 22:02:41 +0530

    IMPLEMENTED TLS DUMP CODE
    Added code in main.go to get TLS info
    of an URL and print it to command line.
    crypto/tls library is used to dial to
    the URL passed and parse it into required
    format and print it to the stdout.
    Errors are handled

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"os"
+        "crypto/tls"
+        "fmt"
+        "log"
+        "os"
 )
 
 func main() {
-	fmt.Fprintln(os.Stderr, "test")
+    conf := &tls.Config{
+        InsecureSkipVerify: true,
+    }
+    if len(os.Args) <= 1 {
+      log.Println("Please Pass the URL")
+      return
+    }
+    url := os.Args[1]
+    conn, err := tls.Dial("tcp", url, conf)
+    if err != nil {
+        log.Println("Error in Dial", err)
+        return
+    }
+    defer conn.Close()
+    certs := conn.ConnectionState().PeerCertificates
+    for _, cert := range certs {
+        fmt.Printf("Issuer: %s\n", cert.Issuer)
+        fmt.Printf("Expiry Date: %s \n", cert.NotAfter)
+        fmt.Printf("Common Name: %s \n", cert.Issuer.CommonName)
+    }
 }
